4386eb050b6251dba80f21e75743c7aba1652967
Harshit Verma <104984173+codealpha9@users.noreply.github.com>
2023-09-06 11:03:44 +0530

    Add files via upload

---
+++ Week 6/Q2.c
@@ -0,0 +1,46 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<pthread.h> 
+ 
+void* summation(void* param) 
+{
+int* arr = (int*)param; 
+int sum = 0; 
+int n = arr[0]; 
+ 
+for(int i = 1;i <= n;i++)
+{
+if(arr[i] > 0)
+sum += arr[i];
+} 
+ 
+return (void*)sum; 
+} 
+ 
+int main(int argc, char const *argv[]) 
+{
+int n; 
+
+printf("Enter the no. of numbers : \n"); 
+scanf("%d",&n); 
+ 
+int* arr = (int*)malloc((n+1)*sizeof(int)); 
+ 
+arr[0] = n;
+ 
+printf("Enter the numbers : \n"); 
+ 
+for(int i= 1;i <= n;i++)
+{
+scanf("%d",&arr[i]);
+}
+ 
+int answer = 0; 
+pthread_t thread; 
+pthread_create(&thread,0,&summation,(void*)arr); 
+pthread_join(thread,(void**)&answer); 
+ 
+printf("Summation of non-negative numbers = %d\n",answer); 
+ 
+return 0; 
+}
---
+++ Week 6/Q4.c
@@ -0,0 +1,103 @@
+#include <errno.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define handle_error_en(en, msg) \
+    do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
+
+volatile int running_threads = 0;
+pthread_t thread[1];
+int numOfElements;
+
+struct Results
+{
+    int sum;
+} Results, Results2;
+
+void *findsum(void *array_ptr)
+{
+    int i; /*counter*/
+    int *elements = (int *)array_ptr;
+    for (i = 0; i < numOfElements; i++)
+    {
+        if (elements[i] % 2 == 0)
+            Results.sum += elements[i];
+        else
+            Results2.sum += elements[i];
+    }
+    running_threads -= 1;
+    return NULL;
+}
+
+int getArrayInput(int n, int **array_ptr)
+{
+    int input;
+    int numberOfElements = 0;
+
+    printf("Creating Dynamic Array...\n-\n");
+
+    for (;;)
+    {
+        printf("Enter a positive value:\nNegative Number to Stop\n-\n");
+
+        if (scanf("%d", &input) != 1)
+        {
+            printf("\nOops, that wasn't an Integer. Let's try filling the array again. Remember INTEGERS only!\n");
+            exit(EXIT_FAILURE);
+        }
+
+        if (input >= 0)
+        {
+            if (numberOfElements == n)
+            {
+                n += 1;
+                *array_ptr = realloc(*array_ptr, n * sizeof(int));
+            }
+
+            (*array_ptr)[numberOfElements++] = input;
+        }
+        else
+        {
+            printf("\nNumber of Integers: %d\n", numberOfElements);
+            break;
+        }
+    }
+
+    return numberOfElements;
+}
+
+void createThreads(int *array_ptr)
+{
+    int s;
+    s = pthread_create(&thread[0], NULL, findsum, (void *)array_ptr);
+
+    if (s != 0)
+    {
+        handle_error_en(s, "pthread_create");
+    }
+
+    running_threads += 1;
+}
+
+int main()
+{
+    int n = 1;
+    int *array_ptr = malloc(n * sizeof(int));
+    numOfElements = getArrayInput(n, &array_ptr);
+
+    createThreads(array_ptr);
+
+    while (running_threads > 0)
+    {
+        sleep(1);
+    }
+
+    printf("\nThe sum of even %d and odd %d\n", Results.sum, Results2.sum);
+
+    free(array_ptr); // Free dynamically allocated memory
+
+    return (0);
+}
